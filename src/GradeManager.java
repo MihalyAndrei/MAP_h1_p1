267a56a1b58d35af6a1959141766c242be187603
Mihaly Cozmuta Andrei <andrei.mihaly@stud.ubbcluj.ro>
2025-10-19 11:56:46 +0300

    template for methodes

--- src/GradeManager.java
+++ src/GradeManager.java
@@ -1,2 +1,23 @@
 public class GradeManager {
+
+    public static int[] getFailedGrades(int[] grades) {
+        //...
+        return new int[0];
+    }
+
+
+    public static double getAverage(int[] grades) {
+        //...
+        return 0.0;
+    }
+
+    public static int[] getRounded(int[] grades) {
+        //...
+        return new int[0];
+    }
+
+    public static int getMaxRounded(int[] grades) {
+        //...
+        return 0;
+    }
 }
